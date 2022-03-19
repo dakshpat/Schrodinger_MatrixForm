508153f60eb4671af4538dbf86f3f19da5f4731c
Daksh <dpatel132@hawk.iit.edu>
2022-03-19 15:14:24 -0500

    Initial Commit

---
+++ Shared/Schrodinger_MatrixFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schrodinger_MatrixFormApp.swift
+//  Shared
+//
+//  Created by Daksh Patel on 3/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Schrodinger_MatrixFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
